b913ad70d1016ef7d0bdee4bcf69e429c04c92e3
Dante Pineda <hotpixels@rocketmail.com>
2012-01-06 14:26:39 +0800

    jan 6 v3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Library::Application.routes.draw do
+  resources :categories
+
   resources :books
 
   # The priority is based upon order of creation:
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120105074042) do
+ActiveRecord::Schema.define(:version => 20120106061748) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
     t.integer  "author_id"
     t.integer  "category_id"
     t.string   "remarks"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categories", :force => true do |t|
+    t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
